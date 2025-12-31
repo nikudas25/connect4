76857e46e58dc22577d967ca953ec5ea344b9700
nikudas25 <dasankit57@gmail.com>
2025-12-31 13:21:37 +0530

    Added a menu screen to the game with highlighting options.

--- main.py
+++ main.py
@@ -4,50 +4,156 @@ import pygame
 from game_logic import *
 from ui import *
 
+MENU = "menu"
+GAME = "game"
+state = MENU
+
 board = create_board()
 game_over = False
 turn = PLAYER_1
 
+mouse_pos = pygame.mouse.get_pos()
+draw_menu(screen, mouse_pos)
 draw_board(screen, board)
 
-while not game_over:
+while True:
+    # pygame.clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
+            sys.exit()
 
-        if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
-            x = event.pos[0]
-            color = RED if turn == PLAYER_1 else YELLOW
-            pygame.draw.circle(screen, color, (x, SQUARE_SIZE // 2), RADIUS)
-            pygame.display.update()
+        #Menu code
+        if state == MENU:
+            mouse_pos = pygame.mouse.get_pos()
+            draw_menu(screen, mouse_pos)
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                action = menu_click(event.pos)
 
-            col = event.pos[0] // SQUARE_SIZE
+                if action == "play":
+                    board = create_board()
+                    turn = PLAYER_1
+                    game_over = False
 
-            row = get_next_open_row(board, col)
+                    state = GAME
+                    draw_board(screen, board)
 
-            if row is not None:
-                animate_piece(screen, board, col, row, turn)
-                place_piece(board, row, col, turn)
-                draw_board(screen, board)
+                elif action == "quit":
+                    pygame.quit()
+                    sys.exit()
 
-                if check_winner(board, turn):
-                    label = FONT.render(
-                        f"Player {turn} wins!", 
-                        True,
-                        RED if turn == PLAYER_1 else YELLOW
-                    )
+        #Game code
+        elif state == GAME:
 
-                    screen.blit(label, (40, 10))
-                    pygame.display.update()
-                    game_over = True
-                
-                turn = PLAYER_2 if turn == PLAYER_1 else PLAYER_1
+            if event.type == pygame.MOUSEMOTION and not game_over:
+                pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+                x = event.pos[0]
+                color = RED if turn == PLAYER_1 else YELLOW
+                pygame.draw.circle(
+                    screen,
+                    color,
+                    (x, SQUARE_SIZE // 2),
+                    RADIUS
+                )
+                pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+                pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+
+                col = event.pos[0] // SQUARE_SIZE
+                row = get_next_open_row(board, col)
+
+                if row is not None:
+                    animate_piece(screen, board, col, row, turn)
+                    place_piece(board, row, col, turn)
+                    draw_board(screen, board)
+
+                    if check_winner(board, turn):
+                        label = FONT.render(
+                            f"Player {turn} wins!",
+                            True,
+                            RED if turn == PLAYER_1 else YELLOW
+                        )
+                        
+                        screen.blit(label, (40, 10))
+                        pygame.display.update()
+                        game_over = True
+
+                    turn = PLAYER_2 if turn == PLAYER_1 else PLAYER_1
 
     if game_over:
-        pygame.time.wait(3000)
+        pygame.time.wait(2000)
+
+
+
+
+
+
+
+#                 if check_winner(board, turn):
+#                     label = FONT.render(
+#                         f"Player {turn} wins!", 
+#                         True,
+#                         RED if turn == PLAYER_1 else YELLOW
+#                     )
+
+#                     screen.blit(label, (40, 10))
+#                     pygame.display.update()
+#                     game_over = True
+                
+#                 turn = PLAYER_2 if turn == PLAYER_1 else PLAYER_1
+
+    # if game_over:
+    #     pygame.time.wait(3000)
+
+
+
+
+
+"""
+Old logic which led to directly to the game.
+"""
+
+# while not game_over:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
+
+#         if event.type == pygame.MOUSEMOTION:
+#             pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+#             x = event.pos[0]
+#             color = RED if turn == PLAYER_1 else YELLOW
+#             pygame.draw.circle(screen, color, (x, SQUARE_SIZE // 2), RADIUS)
+#             pygame.display.update()
+
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+
+#             col = event.pos[0] // SQUARE_SIZE
+
+#             row = get_next_open_row(board, col)
+
+#             if row is not None:
+#                 animate_piece(screen, board, col, row, turn)
+#                 place_piece(board, row, col, turn)
+#                 draw_board(screen, board)
+
+#                 if check_winner(board, turn):
+#                     label = FONT.render(
+#                         f"Player {turn} wins!", 
+#                         True,
+#                         RED if turn == PLAYER_1 else YELLOW
+#                     )
+
+#                     screen.blit(label, (40, 10))
+#                     pygame.display.update()
+#                     game_over = True
+                
+#                 turn = PLAYER_2 if turn == PLAYER_1 else PLAYER_1
+
+    # if game_over:
+    #     pygame.time.wait(3000)
 
--- ui.py
+++ ui.py
@@ -77,3 +77,64 @@ def animate_piece(screen, board, col, row, player):
         pygame.time.delay(20)
         
         
+def draw_menu(screen, mouse_pos):
+    screen.fill(BLACK)
+
+    title = FONT.render("CONNECT 4", True, RED)
+    play_text = FONT.render("PLAY", True, YELLOW)
+    quit_text = FONT.render("QUIT", True, YELLOW)
+
+    play_rect = pygame.Rect(WIDTH // 2 - 150, 300, 300, 70)
+    quit_rect = pygame.Rect(WIDTH // 2 - 150, 400, 300, 70)
+
+    #Hover Detection
+    play_hover = play_rect.collidepoint(mouse_pos)
+    quit_hover = quit_rect.collidepoint(mouse_pos)
+
+    #Colors
+    play_color = BLACK if play_hover else YELLOW
+    quit_color = BLACK if quit_hover else YELLOW
+    play_bg = YELLOW if play_hover else BLACK
+    quit_bg = YELLOW if quit_hover else BLACK
+
+    #Draw title
+    screen.blit(title, (WIDTH // 2 - title.get_width() // 2, 120))
+
+    #Draw play button
+    pygame.draw.rect(screen, play_bg, play_rect, border_radius=12)
+    play_text = FONT.render("PLAY", True, play_color)
+    screen.blit(
+        play_text,
+        (play_rect.centerx - play_text.get_width() // 2,
+        play_rect.centery - play_text.get_height() // 2)
+    )
+
+    #Draw quit button
+    pygame.draw.rect(screen, quit_bg, quit_rect, border_radius = 12)
+    quit_text = FONT.render("QUIT", True, quit_color)
+    screen.blit(
+        quit_text,
+        (quit_rect.centerx - quit_text.get_width() // 2,
+        quit_rect.centery - quit_text.get_height() // 2)
+    )
+
+    # screen.blit(title, (WIDTH // 2 - title.get_width() // 2, 100))
+    # screen.blit(play_text, (WIDTH // 2 - play_text.get_width() // 2, 300))
+    # screen.blit(quit_text, (WIDTH // 2 - quit_text.get_width() // 2, 400))
+
+    pygame.display.update()
+
+def menu_click(pos):
+    # x = pos
+    # y = pos
+
+    play_rect = pygame.Rect(WIDTH // 2 - 100, 300, 200, 70)
+    quit_rect = pygame.Rect(WIDTH // 2 - 100, 400, 200, 70)
+
+    if play_rect.collidepoint(pos):
+        return "play"
+    
+    if quit_rect.collidepoint(pos):
+        return "quit"
+    
+    return None
